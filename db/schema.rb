5e7dec762ef4b93bd4fcf2f2acfaac7c874e42ff
Alexanderia Heflin <alex@gaslight.co>
2015-12-08 20:01:14 -0500

    Add timestamps to tweets

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151208012901) do
+ActiveRecord::Schema.define(version: 20151209004221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tweets", force: :cascade do |t|
-    t.integer "user_id"
-    t.text    "body"
+    t.integer  "user_id"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "tweets", ["user_id"], name: "index_tweets_on_user_id", using: :btree
