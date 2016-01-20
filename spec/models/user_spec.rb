97633adacd82557b0910853464566552766c4b06
Alexanderia Heflin <alex@gaslight.co>
2016-01-20 10:27:11 -0500

    Update user_spec to use FactoryGirl

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,23 @@
 require 'spec_helper'
 
 describe User do
+  let(:user) { FactoryGirl.create(:user, username: "user", password: "password") }
+  let(:follower) { FactoryGirl.create(:user, username: "follower", password: "password") }
+  let(:followed) { FactoryGirl.create(:user, username: "followed", password: "password") }
+
+  before(:each) do
+    FactoryGirl.create(:follow, followed: user, follower: follower)
+    FactoryGirl.create(:follow, followed: followed, follower: user)
+  end
+
   context "with followers" do
     it "responds to .followers" do
-      user = User.create(username: "user", password: "password")
-      user.save
-      follower = User.create(username: "follower", password: "password")
-      follower.save
-      Follow.create(followed: user, follower: follower)
       expect(user.followers).to include(follower)
     end
   end
 
   context "who follows" do
     it "responds to .following" do
-      user = User.create(username: "user", password: "password")
-      user.save
-      followed = User.create(username: "followed", password: "password")
-      followed.save
-      Follow.create(followed: followed, follower: user)
       expect(user.following).to include(followed)
     end
   end
