c94e77b914a0aec753b74e69f5c65a13741c869f
Alexanderia Heflin <alex@gaslight.co>
2016-02-25 19:01:17 -0500

    Update routes to be more mindful of request types

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
   root "sessions#new"
-  resources :users
-
   resources :sessions, only: [:new, :destroy]
+  resources :users
+  resources :tweets, only: [:index, :create, :destroy]
+
   get "logout" => "sessions#destroy", as: "logout"
   get "login" => "sessions#new", as: "login"
   post "authenticate" => "sessions#authenticate", as: "authenticate"
 
-  post '/follow/:user_id', to: 'follows#follow', as: 'follow'
-  post '/unfollow/:follow_id', to: 'follows#unfollow', as: 'unfollow'
+  post '/follow/:user_id', to: 'follows#create', as: 'follow'
+  delete '/unfollow/:id', to: 'follows#destroy', as: 'unfollow'
 
-  post '/favorite/:tweet_id', to: 'favorites#favorite', as: 'favorite'
-  post '/unfavorite/:favorite_id', to: 'favorites#unfavorite', as: 'unfavorite'
-
-  resources :tweets, only: [:index, :create, :destroy]
+  post '/favorite/:tweet_id', to: 'favorites#create', as: 'favorite'
+  delete '/unfavorite/:id', to: 'favorites#destroy', as: 'unfavorite'
 end
