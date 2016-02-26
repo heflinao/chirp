abc78dae25aa06fbb523330dacb38b28f297c9cd
Alexanderia Heflin <alex@gaslight.co>
2016-02-26 15:43:27 -0500

    Clean database around each example

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -12,4 +12,10 @@ RSpec.configure do |config|
       DatabaseCleaner.clean
     end
   end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
